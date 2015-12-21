287cc812d65910d5a180f1b47f1d1fd758db680d
Daniel Altrichter <altrichterdani@gmail.com>
2015-12-21 15:38:42 +0100

    Neuerster Stand

---
+++ app/src/main/java/haw_dating/haw_landshut/de/p2pdatingapp/MainActivity.java
@@ -0,0 +1,20 @@
+package haw_dating.haw_landshut.de.p2pdatingapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
