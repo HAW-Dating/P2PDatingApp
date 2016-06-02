fb7d62d5ecbbb5b124782e86cb46927114a09de5
Daniel <altrichterdani@gmail.com>
2016-06-02 10:57:02 +0200

    Datenspeichern

--- app/src/main/java/de/haw_landshut/haw_dating/p2pdatingapp/MyProfilActivity.java
+++ app/src/main/java/de/haw_landshut/haw_dating/p2pdatingapp/MyProfilActivity.java
@@ -1,12 +1,15 @@
 package de.haw_landshut.haw_dating.p2pdatingapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -28,31 +31,59 @@ public class MyProfilActivity extends Activity implements View.OnTouchListener{
     private ListView drawerList;
     private ArrayAdapter<String> adapter;
 
-    @Override
+    EditText editTextName, editTextStudie, editTextInterests, editTextHometown, editTextPostal_code;
+    int selectedPosition;
+
+    Spinner university,gender, searchSexual_preference;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_profil_main);
 
+
         /* Fuer Spinner Geschlecht */
-        Spinner gender = (Spinner) findViewById(R.id.gender);
+        gender = (Spinner) findViewById(R.id.gender);
         ArrayAdapter<String> adapterGender =
                 new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.sexual_spinner));
         gender.setAdapter(adapterGender);
 
         /* Fuer Spinner Universität */
-        Spinner university = (Spinner) findViewById(R.id.university);
+        university = (Spinner) findViewById(R.id.university);
         ArrayAdapter<String> adapterUniversity =
                 new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.university_spinner));
         university.setAdapter(adapterUniversity);
 
         /* Fuer Spinner Suche-Geschlecht */
-        Spinner searchSexual_preference = (Spinner) findViewById(R.id.sexual_preference);
+        searchSexual_preference = (Spinner) findViewById(R.id.sexual_preference);
         ArrayAdapter<String> adapterSearchGender =
                 new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.sexual_spinner));
         searchSexual_preference.setAdapter(adapterSearchGender);
 
         LinearLayout bildschirm = (LinearLayout) findViewById(R.id.my_profile_activity_linear_layout);
         bildschirm.setOnTouchListener(this);
+
+
+        // SharedPreferences Datei öffnen
+        SharedPreferences preferences = getSharedPreferences("Profildata", 0);
+        // Editorklasse initialisieren
+        editTextName = (EditText)findViewById(R.id.profil_name);
+        // Schlüsselwerte aus der Datei lesen und in Textfelder schreiben
+        editTextName.setText(preferences.getString("name", "Huber Sepp"));
+        editTextStudie = (EditText)findViewById(R.id.profil_studie);
+        editTextStudie.setText(preferences.getString("studie", "Soziale Arbeit"));
+        editTextInterests = (EditText)findViewById(R.id.profil_interests);
+        editTextInterests.setText(preferences.getString("intrests", "Fliegen"));
+        editTextHometown = (EditText)findViewById(R.id.profil_hometown);
+        editTextHometown.setText(preferences.getString("hometown", "Hamburg"));
+        editTextPostal_code = (EditText)findViewById(R.id.profil_postal_code);
+        editTextPostal_code.setText(preferences.getString("postal_code", "22113"));
+
+
+        // Spinner
+        gender.setSelection(preferences.getInt("gender", 0));
+        university.setSelection(preferences.getInt("university", 0));
+        searchSexual_preference.setSelection(preferences.getInt("searchSexual_preference",0));
+
 
         // Navigations Drawer
         drawerList = (ListView) findViewById(R.id.main_lv_menu);
@@ -81,6 +112,35 @@ public class MyProfilActivity extends Activity implements View.OnTouchListener{
             }
         });
     }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+
+        // SharedPreferences Datei öffnen
+        SharedPreferences preferences = getSharedPreferences("Profildata", 0);
+        // Editorklasse initialisieren
+        SharedPreferences.Editor editorPreferences = preferences.edit();
+
+        // Text mit Schlüsselattribut holen und in Editorklasse schreiben
+        editorPreferences.putString("name", editTextName.getText().toString());
+        editorPreferences.putString("studie", editTextStudie.getText().toString());
+        editorPreferences.putString("intrests", editTextInterests.getText().toString());
+        editorPreferences.putString("hometown", editTextHometown.getText().toString() );
+        editorPreferences.putString("postal_code", editTextPostal_code.getText().toString());
+
+        // Spinner
+        selectedPosition = gender.getSelectedItemPosition();
+        editorPreferences.putInt("gender", selectedPosition);
+        selectedPosition = university.getSelectedItemPosition();
+        editorPreferences.putInt("university", selectedPosition);
+        selectedPosition = searchSexual_preference.getSelectedItemPosition();
+        editorPreferences.putInt("searchSexual_preference", selectedPosition);
+
+        // Speichern
+        editorPreferences.apply();
+    }
+
     private void addDrawerItems(){
         adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, (getResources().getStringArray(R.array.drawer_list_menu_array)));
         drawerList.setAdapter(adapter);
